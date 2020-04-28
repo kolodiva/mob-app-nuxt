79f2c498e3b7de4266293c75c9e168edaf60dccd
kolodiva <kolodiva@mail.ru>
2020-04-28 12:55:50 +0300

    ok

--- components/overlay.vue
+++ components/overlay.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center">
     <v-overlay>
-      <v-img src="baner_korona.jpg" width="100vw" contain></v-img>
+      <v-img src="baner_korona_mob.jpg" width="80vw" contain></v-img>
       <v-btn icon @click="$store.commit('switchShowOverlay')">
         <v-icon>mdi-close</v-icon>
       </v-btn>
