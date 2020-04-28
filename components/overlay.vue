3d8f958146e39ec697f7bad7cce256574f323df8
kolodiva <kolodiva@mail.ru>
2020-04-28 15:53:54 +0300

    ok

--- components/overlay.vue
+++ components/overlay.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="text-center">
     <v-overlay>
-      <v-img src="baner_korona_mob.jpg" width="80vw" contain></v-img>
+      <v-img
+        src="baner_korona_mob.jpg"
+        width="80vw"
+        contain
+        @click="
+          $store.commit('switchShowOverlay')
+          $router.push('/view_pdf')
+        "
+      ></v-img>
       <v-btn icon @click="$store.commit('switchShowOverlay')">
         <v-icon>mdi-close</v-icon>
       </v-btn>
---
+++ pages/view_pdf.vue
@@ -0,0 +1,13 @@
+<template>
+  <pdf class="mt-10" src="mf_komplekt_osoby_rezim_raboty.pdf"></pdf>
+</template>
+
+<script>
+import pdf from 'vue-pdf'
+
+export default {
+  components: {
+    pdf,
+  },
+}
+</script>
