c20058c5c28b00507e3b71df10f016831402d008
kolodiva <kolodiva@mail.ru>
2020-05-06 01:03:27 +0300

    ok

--- middleware/load-nomenklator-1.js
+++ middleware/load-nomenklator-1.js
@@ -1,10 +1,10 @@
-// import RSVP from 'rsvp'
+import RSVP from 'rsvp'
 // const consola = require('consola')
 
 export default async function ({ store, from }) {
   // consola.info((req && req.url) || 'no data')
   if (!store.state.isLoaded) {
-    await store.dispatch('nomenklator/loadAll')
+    await RSVP.all([store.dispatch('nomenklator/loadAll')])
     store.commit('FINISH_LOADING')
     // store.dispatch('snackbar/setSnackbar', {
     //   color: 'green',
