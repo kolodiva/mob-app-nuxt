8971483cce02f3ca4fca08f15d77369935725a05
kolodiva <kolodiva@mail.ru>
2020-05-06 01:47:10 +0300

    ok

--- utils/store-utils.js
+++ utils/store-utils.js
@@ -2,7 +2,7 @@
 
 export const getData = async function (url, axios) {
   //const response = await axios.$get(url)
-  const response = await axios.get(url)
+  const response = await axios.$get(url)
   // consola.info(response.data)
   return {
     data: response.data,
