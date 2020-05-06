01549468838c078cef09cdc3c71e29b7d0f6f5cc
kolodiva <kolodiva@mail.ru>
2020-05-06 12:46:28 +0300

    ok

--- middleware/load-nomenklator.js
+++ middleware/load-nomenklator.js
@@ -1,15 +1,15 @@
-// import RSVP from 'rsvp'
+import RSVP from 'rsvp'
 // const consola = require('consola')
 
 export default async function ({ store, from }) {
-  // consola.info(store.state)
-  // if (!store.state.isLoaded) {
-  await store.dispatch('nomenklator/loadAll')
-  // store.commit('FINISH_LOADING')
-  // store.dispatch('snackbar/setSnackbar', {
-  //   color: 'green',
-  //   text: `Top level Nomenklator loaded, Vittorio`,
-  //   timeout: 2000,
-  // })
-  // }
+  // consola.info((req && req.url) || 'no data')
+  if (!store.state.isLoaded) {
+    await RSVP.all([store.dispatch('nomenklator/loadAll')])
+    store.commit('FINISH_LOADING')
+    // store.dispatch('snackbar/setSnackbar', {
+    //   color: 'green',
+    //   text: `Top level Nomenklator loaded, Vittorio`,
+    //   timeout: 2000,
+    // })
+  }
 }
