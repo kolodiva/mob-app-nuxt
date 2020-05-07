2dc069d1187adf64126eedddfed941f230b42c83
kolodiva <kolodiva@mail.ru>
2020-05-07 20:50:43 +0300

    ok

--- api/routes/db.js
+++ api/routes/db.js
@@ -276,7 +276,7 @@ const { Router } = require('express')
     const pass_digest = genPass(req.body.password)
 
     dbpg.query(
-            `insert into users(email, name, phone, password_digest, updated_at, created_at) values( 'afmc2@mail.ru', 'afmc2@mail.ru', '1234567890', '${pass_digest}',now(), now())`
+            `insert into users(email, name, phone, password_digest, updated_at, created_at) values( '${req.body.email}', '${req.body.email}', '${req.body.password}', '${pass_digest}',now(), now())`
           ).then(resp => {
               return res.status(200).send( 'ok' )
           }).catch((err) => {
