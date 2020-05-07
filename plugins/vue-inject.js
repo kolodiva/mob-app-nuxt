bbcababfbe00aed5909630a31fcb6e357d392885
kolodiva <kolodiva@mail.ru>
2020-05-07 11:44:04 +0300

    ok

--- plugins/vue-inject.js
+++ plugins/vue-inject.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 Vue.prototype.$myInjectedFunction = (string) => {
   console.log('This is an example', string)
 }
+
 Vue.prototype.$getCryptoKey = (CryptoJS) => {
   const key1 = CryptoJS.SHA256(
     new Date().getTime().toString() + new Date(1972, 1, 4).getTime().toString()
