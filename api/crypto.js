944b419258ab79d55cf0819e749b2d662207e224
kolodiva <kolodiva@mail.ru>
2020-05-08 07:20:50 +0300

    ok

--- api/crypto.js
+++ api/crypto.js
@@ -1,14 +1,17 @@
-import * as CryptoJS from 'crypto-js';
+//import * as CryptoJS from 'crypto-js';
+import SHA256 from 'crypto-js/sha256';
+import AES from 'crypto-js/aes';
+import encUtf8 from 'crypto-js/enc-utf8';
 const bcrypt = require('bcrypt');
 
 module.exports = {
   matchPass: (key, pass, hash) => {
 
-    const _key = CryptoJS.SHA256(key + '118540800').toString()
+    const _key = SHA256(key + '118540800').toString()
 
-    const bytes = CryptoJS.AES.decrypt(pass, _key)
+    const bytes = AES.decrypt(pass, _key)
 
-    const originalText = CryptoJS.SHA256(CryptoJS.SHA256(bytes.toString(CryptoJS.enc.Utf8)).toString()).toString()
+    const originalText = SHA256(SHA256(bytes.toString(encUtf8)).toString()).toString()
 
     return bcrypt.compareSync(originalText, hash);
   },
@@ -16,7 +19,7 @@ module.exports = {
   genPass: (pass) => {
 
     //const originalText = CryptoJS.SHA256(CryptoJS.SHA256(bytes.toString(CryptoJS.enc.Utf8)).toString()).toString()
-    const originalText = CryptoJS.SHA256(pass).toString()
+    const originalText = SHA256(pass).toString()
     //const originalText = CryptoJS.SHA256(CryptoJS.SHA256('Pp1234567').toString()).toString()
     return bcrypt.hashSync(originalText, 10);
   }
