318a1ed0f45e59a9abd448c9d452a556159467c0
kolodiva <kolodiva@mail.ru>
2020-05-12 01:41:24 +0300

    ok

--- api/db-utils.js
+++ api/db-utils.js
@@ -2,14 +2,14 @@ import { v4 as uuidv4 } from 'uuid';
 
 module.exports = {
 
-  getOrderIdByConnectionId: async (dbpg, req, errList) => {
+  getOrderIdByConnectionId: (dbpg, req, errList) => {
 
     let connectionid = req.cookies.connectionid
     let orderInfo    = {order_id: 0, count_goods: 0}
 
     if (connectionid) {
 
-      await dbpg.query(`
+      dbpg.query(`
         select t2.id order_id, count(t3.id) count_goods
         from connections t1
         left join orders t2 on t1.id=t2.connection_id and t1.remember_token='${connectionid}' and t2.status = 0
@@ -19,12 +19,13 @@ module.exports = {
           if (resp.rowCount > 0) {
             orderInfo.order_id    = resp.rows[0].order_id
             orderInfo.count_goods = resp.rows[0].count_goods
+            return orderInfo;
           }
         }).catch(err => {
           errList.push(err.message)
+          return orderInfo;
         })
     }
-    return orderInfo;
   },
 
   getConnOrder: async (dbpg, req, res, errList) => {
