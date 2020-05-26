7331c902ad3fdb1345291c96921f60d7f7e1af37
kolodiva <kolodiva@mail.ru>
2020-05-26 20:31:35 +0300

    щл

--- components/TheCardList.vue
+++ components/TheCardList.vue
@@ -11,17 +11,107 @@
                 >
               </div>
               <v-list-item-title class="headline mb-1">
-                <v-chip
-                  class="my-3 title"
-                  color=""
-                  text-color="blue darken-4"
-                  ripple
-                  outlined
-                  style="border: 0.5px solid;"
+                <v-menu
+                  v-model="menu"
+                  bottom
+                  right
+                  transition="scale-transition"
+                  origin="top left"
+                  :close-on-content-click="false"
                 >
-                  <v-icon left class="pl-2">mdi-cart-plus</v-icon>
-                  <span>{{ qty_t }}</span>
-                </v-chip>
+                  <template v-slot:activator="{ on }">
+                    <v-chip
+                      class="my-3 title"
+                      color=""
+                      text-color="blue darken-4"
+                      ripple
+                      outlined
+                      style="border: 0.5px solid;"
+                      v-on="on"
+                    >
+                      <v-icon left class="pl-2">mdi-cart-plus</v-icon>
+                      <span>{{ qty_t }}</span>
+                    </v-chip>
+                  </template>
+
+                  <v-card width="300">
+                    <v-card-text class="title pb-0"
+                      >артикул: {{ item.artikul }}</v-card-text
+                    >
+                    <v-chip
+                      class="ma-3 pa-2"
+                      color=""
+                      text-color="blue darken-4"
+                      ripple
+                      outlined
+                      style="border: 0.5px solid; width: 90%;"
+                      v-on="on"
+                    >
+                      <v-icon left class="pl-2">mdi-cart-plus</v-icon>
+                      <span class="title" v-text="qty_t"></span>
+                    </v-chip>
+                    <v-divider />
+                    <v-row>
+                      <v-col cols="9" class="py-0">
+                        <v-chip-group column class="pa-3">
+                          <v-chip
+                            v-for="n in 9"
+                            :key="n"
+                            class="pa-4"
+                            outlined
+                            color="blue darken-4"
+                            ripple
+                            @click="qty_t += `${10 - n}`"
+                          >
+                            <span class="title">{{ 10 - n }}</span>
+                          </v-chip>
+
+                          <v-chip
+                            class="pa-4"
+                            outlined
+                            color="blue darken-4"
+                            ripple
+                          >
+                            <span class="title">0</span>
+                          </v-chip>
+
+                          <v-chip
+                            class="pa-4"
+                            outlined
+                            color="blue darken-4"
+                            ripple
+                            @click="qty_t += '.'"
+                          >
+                            <span class="title">.</span>
+                          </v-chip>
+                        </v-chip-group>
+                      </v-col>
+                      <v-col class="px-0" cols="3">
+                        <v-chip-group column class="">
+                          <v-chip
+                            class="pa-3"
+                            outlined
+                            color="blue darken-4"
+                            ripple
+                            @click="qty_t = '0'"
+                          >
+                            <span class="title red--text">СЕ</span>
+                          </v-chip>
+                          <v-spacer />
+                          <v-chip
+                            class="pa-3"
+                            outlined
+                            color="blue darken-4"
+                            ripple
+                            @click="menu = !menu"
+                          >
+                            <span class="title green--text">OK</span>
+                          </v-chip>
+                        </v-chip-group>
+                      </v-col>
+                    </v-row>
+                  </v-card>
+                </v-menu>
               </v-list-item-title>
 
               <v-list-item-subtitle>{{ item.name }}</v-list-item-subtitle>
@@ -113,6 +203,8 @@
 export default {
   props: ['item'],
   data: () => ({
+    on: false,
+    menu: false,
     length: 3,
     window: 0,
     qty_t: 123,
