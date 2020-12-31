8fafb8c7f85a6df44df4946b8e50c3c8535d7ddc
Kolodiva <Kolodiva@mail.ru>
2020-12-31 07:22:56 +0300

    ok

--- store/snackbar.js
+++ store/snackbar.js
@@ -1,13 +1,21 @@
 export const state = () => ({
   snackbars: [],
+  showMainDisclaimer: true,
 })
 
 export const mutations = {
   SET_SNACKBAR(state, snackbar) {
     state.snackbars = state.snackbars.concat(snackbar)
   },
+  SET_SHOW_MAIN_DISCLAIMER(state, val) {
+    state.showMainDisclaimer = val;
+  },
 }
-
+export const getters = {
+  showMainDisclaimer: (state) => {
+    return state.showMainDisclaimer;
+  },
+},
 export const actions = {
   setSnackbar({ commit }, snackbar) {
     snackbar.showing = true
