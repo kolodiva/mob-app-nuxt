1adc899cc64b49a59c878179410eee8768dd886f
kolodiva <kolodiva@mail.ru>
2021-01-18 21:47:10 +0300

    ok

--- server/index_mob.js
+++ server/index_mob.js
@@ -19,7 +19,7 @@ async function start() {
     host = 'localhost'; port = 6000;
   } else {
     //const { host, port } = nuxt.options.server;
-    host = '192.168.1.41'; port = 3000;
+    host = '192.168.1.41'; port = 5000;
   }
 
   await nuxt.ready()
