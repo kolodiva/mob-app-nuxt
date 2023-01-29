d927f44776d74bd8e9d9dbdb138690ceff8a3698
kolodiva <kolodiva@mail.ru>
2023-01-29 22:19:17 +0300

    ok

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,22 @@
 <template>
-  <v-row class="px-2" style="">
+  <v-row class="px-2" style="margin-top: 45px;">
     <v-col
       v-for="pos in nomenklator"
       :key="pos.id"
       class="d-flex child-flex"
       cols="6"
     >
-      <n-link :to="`catalog/${pos.file_name}`" style="text-decoration: none">
+      <n-link :to="`catalog/${pos.file_name}`" style="text-decoration: none;">
         <v-card>
           <v-avatar tile class="grey lighten-2 ma-0" width="100%" height="140">
             <img
               :src="`/index_pic/${pos.file_name}.png`"
-              style="max-height: 120px; max-width: 140px"
+              style="max-height: 120px; max-width: 140px;"
               :alt="`${pos.name}`"
             />
           </v-avatar>
           <v-card-text class="text--primary pa-2">
-            <div class="text-center" style="height: 45px; overflow: hidden">
+            <div class="text-center" style="height: 45px; overflow: hidden;">
               {{ pos.name }}
             </div>
           </v-card-text>
