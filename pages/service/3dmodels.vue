ca80c07c4ea2ecc4a4062faa2775d06ffa8e4f04
kolodiva <kolodiva@mail.ru>
2025-07-07 23:50:34 +0300

    щл

--- pages/service/3dmodels.vue
+++ pages/service/3dmodels.vue
@@ -8,8 +8,8 @@
         БАЗИС-Мебельщик, 3D Модели
       </h1>
 
-  <v-card shaped raised class="mb-3" max-height="210" min-height="210">
-        <v-card flat shaped>
+  <v-card shaped raised class="mb-3">
+        <v-card flat shaped class="pa-2">
           
           <v-tooltip top>
             <template v-slot:activator="{ on, attrs }">
@@ -30,14 +30,219 @@
             <span>Загрузить 3D модели</span>
           </v-tooltip>
 
-          <v-card-subtitle style="max-width: 140px"
+          <v-card-subtitle style=""
             ><span>Направляющие шариковые полного выдвижения H=35 мм BBS.35</span>
           </v-card-subtitle>
         
         </v-card>
   </v-card>
 
-    </v-col>
+  <v-card shaped raised class="mb-3">
+        <v-card flat shaped class="pa-2">
+          
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img
+                class="mx-auto"
+                max-width="72"
+                src="/icon-zip.png"
+                style="cursor: pointer"
+                v-bind="attrs"
+                v-on="on"
+                @click="
+                  downloadResource(
+                    'https://www.newfurnitura.ru/news/napravl_skryt_typa_clipon_polnogo_vidvigenia_s_dovodchikom_s_sinhr_3dregulirovkoi_CS.0003D.zip'
+                  )
+                "
+              />
+            </template>
+            <span>Загрузить 3D модели</span>
+          </v-tooltip>
+
+          <v-card-subtitle style=""
+            ><span>CS.000.3D Направляющие скрытого типа Clip-on полного выдв. с доводчиком с синх. 3D регулировкой</span>
+          </v-card-subtitle>
+        
+        </v-card>
+  </v-card>
+
+
+  <v-card shaped raised class="mb-3">
+        <v-card flat shaped class="pa-2">
+          
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img
+                class="mx-auto"
+                max-width="72"
+                src="/icon-zip.png"
+                style="cursor: pointer"
+                v-bind="attrs"
+                v-on="on"
+                @click="
+                  downloadResource(
+                    'https://www.newfurnitura.ru/news/MF_napravl_sharikovie_skrytogo_typaDMI_803SHD.zip'
+                  )
+                "
+              />
+            </template>
+            <span>Загрузить 3D модели</span>
+          </v-tooltip>
+
+          <v-card-subtitle style=""
+            ><span>MF Направляющие шариковые скрытого типа DMI-803SHD</span>
+          </v-card-subtitle>
+        
+        </v-card>
+  </v-card>
+
+  <v-card shaped raised class="mb-3">
+        <v-card flat shaped class="pa-2">
+          
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img
+                class="mx-auto"
+                max-width="72"
+                src="/icon-zip.png"
+                style="cursor: pointer"
+                v-bind="attrs"
+                v-on="on"
+                @click="
+                  downloadResource(
+                    'https://www.newfurnitura.ru/news/MF_Fragmenty_petel_52_gramma_MF_201_45gr_90gr.zip'
+                  )
+                "
+              />
+            </template>
+            <span>Загрузить 3D модели</span>
+          </v-tooltip>
+
+          <v-card-subtitle style=""
+            ><span>MF Фрагменты петель 52 грамма, MF-201, 45гр, 90гр</span>
+          </v-card-subtitle>
+        
+        </v-card>
+  </v-card>
+
+  <v-card shaped raised class="mb-3">
+        <v-card flat shaped class="pa-2">
+          
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img
+                class="mx-auto"
+                max-width="72"
+                src="/icon-zip.png"
+                style="cursor: pointer"
+                v-bind="attrs"
+                v-on="on"
+                @click="
+                  downloadResource(
+                    'https://www.newfurnitura.ru/news/MF_Napravljajushhie_CS000POSCL2D_polnogo_vydv_otkryvanie_nazh_s_dovod_s_sinhr_2D.zip'
+                  )
+                "
+              />
+            </template>
+            <span>Загрузить 3D модели</span>
+          </v-tooltip>
+
+          <v-card-subtitle style=""
+            ><span>MF Направляющие CS.000.PO.SCL.2D полного выдв. открывание наж. с довод. с синхр, 2D</span>
+          </v-card-subtitle>
+        
+        </v-card>
+  </v-card>
+
+  <v-card shaped raised class="mb-3">
+        <v-card flat shaped class="pa-2">
+          
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img
+                class="mx-auto"
+                max-width="72"
+                src="/icon-zip.png"
+                style="cursor: pointer"
+                v-bind="attrs"
+                v-on="on"
+                @click="
+                  downloadResource(
+                    'https://www.newfurnitura.ru/news/MF_Napravljajushhie_BBS45_sharikovye_polnogo_vydvizhenija_H45mm.zip'
+                  )
+                "
+              />
+            </template>
+            <span>Загрузить 3D модели</span>
+          </v-tooltip>
+
+          <v-card-subtitle style=""
+            ><span>MF Направляющие BBS.45 шариковые полного выдвижения H=45 мм</span>
+          </v-card-subtitle>
+        
+        </v-card>
+  </v-card>
+
+  <v-card shaped raised class="mb-3">
+        <v-card flat shaped class="pa-2">
+          
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img
+                class="mx-auto"
+                max-width="72"
+                src="/icon-zip.png"
+                style="cursor: pointer"
+                v-bind="attrs"
+                v-on="on"
+                @click="
+                  downloadResource(
+                    'https://www.newfurnitura.ru/news/MF_Napravlyayushchie_CSS_000_H_nepolnogo_vydvizheniya_s_dovodchikom.zip'
+                  )
+                "
+              />
+            </template>
+            <span>Загрузить 3D модели</span>
+          </v-tooltip>
+
+          <v-card-subtitle style=""
+            ><span>MF Направляющие CSS.000.H неполного выдвижения с доводчиком</span>
+          </v-card-subtitle>
+        
+        </v-card>
+  </v-card>
+
+  <v-card shaped raised class="mb-3">
+        <v-card flat shaped class="pa-2">
+          
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img
+                class="mx-auto"
+                max-width="72"
+                src="/icon-zip.png"
+                style="cursor: pointer"
+                v-bind="attrs"
+                v-on="on"
+                @click="
+                  downloadResource(
+                    'https://www.newfurnitura.ru/news/MF_Napravlyayushchie_CSS_000_P_nepolnogo_vydvizheniya_s_dovodchikom.zip'
+                  )
+                "
+              />
+            </template>
+            <span>Загрузить 3D модели</span>
+          </v-tooltip>
+
+          <v-card-subtitle style=""
+            ><span>MF Направляющие CSS.000.P неполного выдвижения с доводчиком</span>
+          </v-card-subtitle>
+        
+        </v-card>
+  </v-card>
+
+
+</v-col>
 
   </v-container>
 
