ade483f57f3d37c937447c54a9e920290ce63eea
kolodiva <kolodiva@mail.ru>
2025-07-07 11:05:47 +0300

    ok

---
+++ pages/service/3dmodels.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container
+    fill-height
+    class="pa-5 mt-10 font-weight-light headline text-justify"
+  >
+    <content> <p>Данные для системы Базис доступны только на стационарном Сайте.</p> </content>
+  </v-container>
+</template>
+<script>
+export default {
+  beforeCreate() {
+    this.$store.commit('setHeaderName', 'Архивы фурнитуры для использования в системе автоматического проектирования мебели БАЗИС-Мебельщик на базе фурнитуры компании МФ-Комплект.')
+  },
+}
+</script>
